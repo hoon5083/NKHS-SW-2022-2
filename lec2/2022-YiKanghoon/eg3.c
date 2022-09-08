ad8c434386e4e273895abe612be3f2be629ced9c
Yi Kanghoon <hoon5083@gmail.com>
2022-09-08 10:34:59 +0900

    feat: examples

---
+++ lec2/2022-YiKanghoon/eg3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+  int i = 1;
+  char c = 'a';
+
+  printf("%d %c", i, c);
+}
---
+++ lec2/2022-YiKanghoon/eg6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+  int age = 100;
+  printf("Age: %d", age);
+  return 0;
+}
