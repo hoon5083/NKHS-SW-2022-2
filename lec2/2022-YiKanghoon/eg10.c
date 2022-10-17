ba2746db104ac4d87f1dbb9f846206aaba95e44b
Yi Kanghoon <hoon5083@gmail.com>
2022-10-18 05:02:55 +0900

    feat: 3차시 예제 파일

---
+++ lec2/2022-YiKanghoon/eg10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+  int intNum = 0;
+  printf("%d\n", intNum++);
+  printf("%d\n", ++intNum);
+  printf("%d\n", --intNum);
+  printf("%d\n", intNum--);
+}
--- lec2/2022-YiKanghoon/eg2(error).c
+++ lec2/2022-YiKanghoon/eg2(error).c
@@ -8,7 +8,7 @@ int 1sum;
 num1 = 1;
 _Num2 = 2;
 nuM1 = 3;
-1sum = num1 + _Num2 + num3;
+1sum = num1 + _num2 + num3;
 printf("sum1: %d\n", 1sum);
 return 0;
 }
